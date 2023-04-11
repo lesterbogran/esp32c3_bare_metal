ba12d02c278b7d4a2230f2507ddfd64dfff6377e
lesterbogran <lesterbogran@yahoo.com>
2023-04-10 20:07:30 -0600

    Simple build scripts for windows and linux

---
+++ example/build.bat
@@ -0,0 +1,5 @@
+@echo off
+riscv-none-embed-gcc -march=rv32imc -c driver/wdt.c
+riscv-none-embed-gcc -march=rv32imc -c main.c start.S vectors.S -I./driver
+riscv-none-embed-gcc -o ejemplo -march=rv32imc --specs=nano.specs --specs=nosys.specs -nostartfiles -T ld/esp32c3.ld -T ld/common.ld -T ld/romfuncs.ld main.o start.o vectors.o wdt.o
+riscv-none-embed-objcopy -O binary ejemplo ejemplo.bin
